0385089211ee198435bcd7b44a0faf30a3112730
alsen <alsenaligullu@gmail.com>
2023-07-04 17:12:57 +0100

    created scraper to get book data from books to scrape

--- scrape_for_books.py
+++ scrape_for_books.py
@@ -1,18 +1,25 @@
 from bs4 import BeautifulSoup
 import requests
+import csv
+from datetime import datetime
 
 
 def scrape_data():
     url = "https://books.toscrape.com/catalogue/page-*.html"
     number_of_pages = 51
+    current_datetime = datetime.now().strftime("%y%m%d-%H%M")
 
-    # Creating the file to save the book data
-    with open(f"fetched_books.csv", 'w', encoding='utf-8') as f:
+    # Creating a unique csv file to save the book data
+    with open(f"fetched_books_{current_datetime}.csv", 'w', encoding='utf-8') as f:
+        writer = csv.writer(f)
+        writer.writerow(['title', 'price', 'rating', 'stock'])
+
         for page in range(1,number_of_pages):
             page_url = url.replace('*', str(page))
             webpage = requests.get(page_url).text
             soup = BeautifulSoup(webpage, "lxml")
             books = soup.findAll(class_="product_pod") 
+            
             for book in books:
                 book_title = book.h3.a['title']
                 book_price = book.find(class_="price_color").text
